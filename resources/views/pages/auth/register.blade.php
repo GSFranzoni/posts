b1dcf23ee47f7093bd80b461478b8352d9d1463b
guilherme.franzoni <guilherme.franzoni@unifagoc.edu.br>
2022-06-27 20:46:13 -0300

    feat: first commit

---
+++ resources/views/pages/auth/register.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
+                <div class="mb-4">
+                    <label for="name" class="sr-only">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                </div>
+                <div class="mb-4">
+                    <label for="username" class="sr-only">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                </div>
+                <div class="mb-4">
+                    <label for="repeat-password" class="sr-only">Password again</label>
+                    <input type="repeat-password" name="repeat-password" id="repeat-password" placeholder="Repeat your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                </div>
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
