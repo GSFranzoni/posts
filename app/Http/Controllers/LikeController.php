21365b8c988fd07b4808a430599721125886636e
guilherme.franzoni <guilherme.franzoni@unifagoc.edu.br>
2022-06-28 19:33:43 -0300

    feat: send email when a user likes a post

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,20 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class LikeController extends Controller
 {
     public function store(Post $post, Request $request)
     {
         $user = $request->user();
+
         if ($post->likedBy($user)) {
             return response(null, 409);
         }
+
         $post->likes()->create([
             'user_id' => $user->id
         ]);
+
+        Mail::to($post->user)->send(new PostLiked($user, $post));
+
         return back();
     }
 
---
+++ app/Mail/PostLiked.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PostLiked extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * @var Post
+     */
+    public $post;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Post $post)
+    {
+        $this->user = $user;
+        $this->post = $post;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('email.posts.post_liked')
+            ->subject('Someone liked your post');
+    }
+}
