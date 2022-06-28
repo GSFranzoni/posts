0931d891e11c8232e9f3aa34622bd51400796325
guilherme.franzoni <guilherme.franzoni@unifagoc.edu.br>
2022-06-28 12:53:12 -0300

    feat: post form

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -21,3 +22,5 @@ Route::post('logout', [ LogoutController::class, 'index' ])->name('logout');
 Route::get('dashboard', [ DashboardController::class, 'index' ])
     ->name('dashboard')
     ->middleware('auth');
+
+Route::get('posts', [ PostController::class, 'index' ])->name('posts');
